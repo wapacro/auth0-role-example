83800745508ac9699d0eb9dd186a306cf8c9ed81
Roman Ackermann <roman.ackermann@okta.com>
2023-04-10 23:54:05 +0200

    Added Auth0

--- src/App.vue
+++ src/App.vue
@@ -15,8 +15,7 @@ const navigation = [
       <nav class="flex items-center justify-between p-6 px-8" aria-label="Global">
         <div class="flex flex-1">
           <a href="#" class="-m-1.5 p-1.5">
-            <span class="sr-only">Your Company</span>
-            <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+            <img class="h-8 w-auto" src="/favicon.png" alt="" />
           </a>
         </div>
         <div class="flex gap-x-12">
