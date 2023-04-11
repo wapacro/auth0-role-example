91b7921debe7e16e434f8de1996d86658aaa4ad1
Roman Ackermann <roman.ackermann@okta.com>
2023-04-11 10:57:51 +0200

    Added logout capability

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import OrganizationView from '../views/OrganizationView.vue'
 import ShopView from '../views/ShopView.vue'
+import LogoutView from '../views/LogoutView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: '/shop',
       name: 'shop',
       component: ShopView
+    },
+    {
+      path: '/logout',
+      name: 'logout',
+      component: LogoutView
     }
     /*{
       path: '/about',
---
+++ src/views/LogoutView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { useAuth0 } from "@auth0/auth0-vue";
+
+const { logout } = useAuth0();
+
+logout();
+</script>
+
+<template>
+  <h2>Logging out...</h2>
+</template>
+
+<style scoped>
+
+</style>
