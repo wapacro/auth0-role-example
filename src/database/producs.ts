7ee981a5100141ced91e5330483c34e0eda255af
Roman Ackermann <roman.ackermann@okta.com>
2023-04-11 13:00:37 +0200

    Added dummy database for products

---
+++ src/database/producs.ts
@@ -0,0 +1,16 @@
+export default [
+    {
+        id: 1,
+        name: 'Product 1',
+        image: 'https://example.com/image.jpg',
+        price: 'CHF XX',
+        requiredRole: ['personal', 'emerging']
+    },
+    {
+        id: 2,
+        name: 'Product 2',
+        image: 'https://example.com/image2.jpg',
+        price: 'CHF XX',
+        requiredRole: ['personal', 'emerging', 'enterprise']
+    }
+];
