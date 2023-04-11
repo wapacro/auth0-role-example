73f1dc45928dbf9c65d9804a7616c35c5094988c
Roman Ackermann <roman.ackermann@okta.com>
2023-04-11 09:26:36 +0200

    Added dummy database for organizations & memberships

---
+++ src/database/organizationUsers.ts
@@ -0,0 +1,12 @@
+export default [
+    {
+        id: 1,
+        user: 'user@domain.tld',
+        organization: 'org_XXX'
+    },
+    {
+        id: 2,
+        user: 'user@domain.tld',
+        organization: 'org_XXX'
+    }
+];
---
+++ src/database/organizations.ts
@@ -0,0 +1,12 @@
+export default [
+    {
+        id: 'org_XXX',
+        name: 'XXX',
+        image: 'https://placehold.co/200x200?text=XXX'
+    },
+    {
+        id: 'org_YYY',
+        name: 'YYY',
+        image: 'https://placehold.co/200x200?text=YYY'
+    }
+];
