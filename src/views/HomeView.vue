abd778fdff2bbd0b45ba4dc6b41e3b229fdaf7dd
Roman Ackermann <roman.ackermann@okta.com>
2023-04-11 11:22:25 +0200

    Updated home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,29 @@
 <script setup lang="ts">
+import { useAuth0 } from "@auth0/auth0-vue";
+import { onBeforeRouteUpdate, useRouter } from "vue-router";
 
+const { isAuthenticated, loginWithRedirect } = useAuth0();
+const router = useRouter();
+
+onBeforeRouteUpdate(() => {
+  // If the user is already logged in,
+  // redirect directly to shop page
+  if (isAuthenticated.value) {
+    router.push('/shop');
+  }
+});
 </script>
 
 <template>
-  <div class="hidden sm:mb-8 sm:flex sm:justify-center">
-    <div class="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-      Announcing our next round of funding.
-      <a href="#" class="font-semibold text-indigo-600"><span class="absolute inset-0" aria-hidden="true" />Read more
-        <span aria-hidden="true">&rarr;</span></a>
-    </div>
-  </div>
   <div class="text-center">
-    <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Data to enrich your online business</h1>
-    <p class="mt-6 text-lg leading-8 text-gray-600">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
+    <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Do amazing things with Customer Identity Cloud</h1>
+    <p class="mt-6 text-lg leading-8 text-gray-600">
+      Okta Customer Identity Cloud aka Auth0 is your one-stop shop for all things identity. This sample app uses Auth0's Organization
+      feature to allow your customers to select their desired role and dynamically adjusts the listings in the demo shop.
+    </p>
     <div class="mt-10 flex items-center justify-center gap-x-6">
-      <a href="#" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get started</a>
-      <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Learn more
+      <a href="#" @click="loginWithRedirect()" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get started</a>
+      <a href="https://auth0.com/docs/manage-users/organizations" target="_blank" class="text-sm font-semibold leading-6 text-gray-900">Learn more
         <span aria-hidden="true">→</span></a>
     </div>
   </div>
